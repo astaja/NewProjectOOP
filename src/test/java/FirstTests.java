ef525b6c7af300a4aff3b9e27e764fe0eabdeae3
astaja <asta.jakx@gmail.com>
2023-10-04 09:12:43 +0300

    Uncommenting 1st positive test

--- src/test/java/FirstTests.java
+++ src/test/java/FirstTests.java
@@ -48,30 +48,30 @@ public class FirstTests {
         Helper.driver.quit();
     }
 
-//    @Test
-//    public void addNewAdTest() { // Nr 1 positive Test
-//        driver.findElement(By.id("contacts_name")).sendKeys("Nammeeee");
-//        driver.findElement(By.id("contacts_email")).sendKeys("asdefgmhejj@gmail.com");
-//        driver.findElement(By.id("contacts_phone")).sendKeys("+37061111111");
-//        driver.findElement(By.id("condition_id")).click();                                             // condition
-//        driver.findElement(By.xpath("//*[@id=\"condition_id\"]/option[2]")).click();
-//        driver.findElement(By.id("color_id")).click();                                                // color
-//        driver.findElement(By.xpath("//*[@id=\"color_id\"]/option[3]")).click();
-//        driver.findElement(By.id("width")).sendKeys("1000");
-//        driver.findElement(By.id("length")).sendKeys("1000");
-//        driver.findElement(By.id("fk_place_cities_id[]")).click();
-//        driver.findElement(By.xpath("//*[@id=\"fk_place_cities_id[]\"]/optgroup[1]/option[2]")).click();
-//        driver.findElement(By.id("title_lt")).sendKeys("titeltitle");
-//        driver.findElement(By.id("comments_lt")).sendKeys("descriptiondescription");
-//        driver.findElement(By.id("sell_price_eur")).sendKeys("99999");
-//        String path = System.getProperty("user.dir") + "/TestPictures/logo.png"; // upload pic
-//        WebElement upload_file = driver.findElement(By.id("filedata"));
-//        upload_file.sendKeys(path);
-//        driver.findElement(By.id("video_url")).sendKeys("https://www.youtube.com/watch?v=AfKZclMWS1U&pp=ygUOZ3plZ296IHlvdXR1YmU%3D&ab_channel=trouchelle");
-//        driver.findElement(By.xpath("//*[@id=\"main_form\"]/div[3]/div/input")).click();
-//        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("superpassword3456");
-//        driver.findElement(By.className("submit-button")).click();
-//    }
+    @Test
+    public void addNewAdTest() { // Nr 1 positive Test
+        Helper.driver.findElement(By.id("contacts_name")).sendKeys("Nammeeee");
+        Helper.driver.findElement(By.id("contacts_email")).sendKeys("asdefgmhejj@gmail.com");
+        Helper.driver.findElement(By.id("contacts_phone")).sendKeys("+37061111111");
+        Helper.driver.findElement(By.id("condition_id")).click();                                             // condition
+        Helper.driver.findElement(By.xpath("//*[@id=\"condition_id\"]/option[2]")).click();
+        Helper.driver.findElement(By.id("color_id")).click();                                                // color
+        Helper.driver.findElement(By.xpath("//*[@id=\"color_id\"]/option[3]")).click();
+        Helper.driver.findElement(By.id("width")).sendKeys("1000");
+        Helper.driver.findElement(By.id("length")).sendKeys("1000");
+        Helper.driver.findElement(By.id("fk_place_cities_id[]")).click();
+        Helper.driver.findElement(By.xpath("//*[@id=\"fk_place_cities_id[]\"]/optgroup[1]/option[2]")).click();
+        Helper.driver.findElement(By.id("title_lt")).sendKeys("titeltitle");
+        Helper.driver.findElement(By.id("comments_lt")).sendKeys("descriptiondescription");
+        Helper.driver.findElement(By.id("sell_price_eur")).sendKeys("99999");
+        String path = System.getProperty("user.dir") + "/TestPictures/logo.png"; // upload pic
+        WebElement upload_file = Helper.driver.findElement(By.id("filedata"));
+        upload_file.sendKeys(path);
+        Helper.driver.findElement(By.id("video_url")).sendKeys("https://www.youtube.com/watch?v=AfKZclMWS1U&pp=ygUOZ3plZ296IHlvdXR1YmU%3D&ab_channel=trouchelle");
+        Helper.driver.findElement(By.xpath("//*[@id=\"main_form\"]/div[3]/div/input")).click();
+        Helper.driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("superpassword3456");
+        Helper.driver.findElement(By.className("submit-button")).click();
+    }
     @Test
     public void secondTest() {
         Register r = new Register("nicename", "uvdeve@gmail.com", "+37060000000", "super nice",
